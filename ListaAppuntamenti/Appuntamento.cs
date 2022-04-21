7678c9056e40465a751b16b794a9784348295280
P.angelino <p.angelinodaniele@gmail.com>
2022-04-21 20:14:10 +0200

    Add project files.

---
+++ ListaAppuntamenti/Appuntamento.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agenda
+{
+    internal class Appuntamento
+    {
+
+        private static DateTime dataOraAttuale = DateTime.Now;
+        private string nome;
+        private string localitaAppuntamento;
+        private DateTime dataTime;
+
+        public Appuntamento()
+        {
+        }
+        public Appuntamento(string nome, string localitaAppuntamento, DateTime dataTime)
+        {
+            this.nome = nome;
+            this.localitaAppuntamento = localitaAppuntamento;
+            this.dataTime = dataTime;
+            
+            
+
+            if(dataTime < dataOraAttuale)
+            {
+                throw new ArgumentOutOfRangeException("dataTime", "Mi dispiace non puoi andare indietro nel tempo");
+            }
+        }
+
+        
+
+        public void setDataTime(DateTime dataTimePassata)
+        {
+            dataTime = dataTimePassata;
+            if (dataTime < dataOraAttuale)
+            {
+                throw new ArgumentOutOfRangeException("dataTime", "Mi dispiace non puoi andare indietro nel tempo");
+                
+                return;
+            }
+        }
+
+
+        public string getNome()
+        {
+            return nome;
+        }
+
+        public string getLocalitaAppuntamento()
+        {
+            return localitaAppuntamento;
+        }
+
+
+        public void setNome(string nome)
+        {
+            this.nome = nome;       
+        }
+
+
+        public void setLocalitaAppuntamento(string localitaAppuntamento)
+        {
+            this.localitaAppuntamento = localitaAppuntamento;
+        }
+
+
+        public void StampaOggetto()
+        {
+            Console.WriteLine("----LA TUA AGENDA---- \n");
+            Console.WriteLine("Nome della persona con cui hai l'appuntamento: " + this.nome);
+            Console.WriteLine("Localita del tuo appuntamento: " + this.localitaAppuntamento);
+            Console.WriteLine("data e orario del tuo appuntamento: " + this.dataTime);
+            Console.WriteLine();    
+        }
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+}
---
+++ ListaAppuntamenti/Program.cs
@@ -0,0 +1,65 @@
+﻿using Agenda;
+
+
+List<Appuntamento> listaAppuntamenti=new List<Appuntamento>();
+string nome="";
+string localitaAppuntamento="";
+DateTime data =new DateTime();
+
+Console.Write("Inserisci il numero di appuntamenti che desideri ");
+int counter = int.Parse(Console.ReadLine());
+
+for(int i = 0; i < counter; i++) {
+
+    Console.Write("Inserisci il nome dell'appuntamento: ");
+    nome = Console.ReadLine();
+    Console.Write("Inserisci la localita dell'appuntamento: ");
+    localitaAppuntamento = Console.ReadLine();
+    
+
+
+    bool controlloData=false;
+    while (controlloData == false)
+    {
+        try
+        {
+            Console.WriteLine();
+            Console.Write("Inserisci la data con orario: ");
+            data = DateTime.Parse(Console.ReadLine());
+             
+            
+
+
+            listaAppuntamenti.Add(new Appuntamento(nome, localitaAppuntamento, data));
+
+            controlloData = true;
+
+
+        }
+        catch (ArgumentOutOfRangeException e)
+        {
+            Console.Write(e.Message);
+
+        }
+    }
+    
+}
+
+foreach (Appuntamento ogniAppuntamento in listaAppuntamenti)
+{
+    ogniAppuntamento.StampaOggetto();
+}
+
+
+
+
+
+
+   
+    
+    
+    
+
+
+
+    
